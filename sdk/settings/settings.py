3c76bdb9774065fd29fab5c3f4a562df67e3d7b5
Johannes Maria Frank <jmfrank63@gmail.com>
2019-01-17 11:36:07 +0000

    Initial tests

--- sdk/settings/settings.py
+++ sdk/settings/settings.py
@@ -13,6 +13,14 @@ class Settings:
     def __init__(self, path=__BOX_APP_SETTINGS__):
         settings = self.read_settings_file(path)
         self.client_id = settings['boxAppSettings']['clientID']
+        self.client_secret = settings['boxAppSettings']['clientSecret']
+        self.public_key_id = settings['boxAppSettings']['appAuth']['publicKeyID']
+        self.private_key = settings['boxAppSettings']['appAuth']['privateKey']
+        self.passphrase = settings['boxAppSettings']['appAuth']['passphrase']
+        self.enterprise_id = settings['enterpriseID']
+        self.webhooks_primary_key = settings['webhooks']['primaryKey']
+        self.webhooks_secondary_key = settings['webhooks']['secondaryKey']
+
 
     def read_settings_file(self, path):
         ''' Reads a box settings file
